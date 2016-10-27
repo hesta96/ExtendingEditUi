e9c1a437de10f3967a36f9d2bfe61663f9723901
Henrik Fransas <hesta96@hotmail.com>
2016-10-27 21:04:02 +0200

    Fixed more tests

---
+++ ExtendingEditUi.Test/ImageFileControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.Web.Routing;
+using ExtendingEditUi.Controllers;
+using ExtendingEditUi.Models.Media;
+using ExtendingEditUi.Models.ViewModels;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace ExtendingEditUi.Test
+{
+    [TestFixture]
+    public class ImageFileControllerTests
+    {
+        [Test]
+        public void CanGetViewModelForIndexAction()
+        {
+            var urlResolver = A.Fake<UrlResolver>();
+            A.CallTo(() => urlResolver.GetUrl(new ContentReference(1))).Returns("/myimage.jpg");
+
+            var imageFileController = new ImageFileController(urlResolver);
+
+            var imageFile = new ImageFile() {Name = "ImageFile", Copyright = "Copyright", ContentLink = new ContentReference(1)};
+
+            var result = imageFileController.Index(imageFile) as PartialViewResult;
+
+            var model = result.Model as ImageViewModel;
+
+            Assert.That(model.Name, Is.EqualTo("ImageFile"));
+            Assert.That(model.Copyright, Is.EqualTo("Copyright"));
+            Assert.That(model.Url, Is.EqualTo("/myimage.jpg"));
+
+        }
+    }
+}
