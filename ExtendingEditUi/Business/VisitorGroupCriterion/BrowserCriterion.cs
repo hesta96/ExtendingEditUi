4e6f740fd15f1b5e8652ab81a7346d521a7d6eed
Henrik Fransas <hesta96@hotmail.com>
2016-11-19 18:12:20 +0100

    Implemented custom VisitorGroupCriteria

---
+++ ExtendingEditUi/Business/VisitorGroupCriterion/BrowserCriterion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using EPiServer.Filters;
+using EPiServer.Personalization.VisitorGroups;
+using ExtendingEditUi.Models.VisitorGroupCriterion;
+
+namespace ExtendingEditUi.Business.VisitorGroupCriterion
+{
+    [VisitorGroupCriterion(
+        Category = "User Criteria",
+        DisplayName = "Browser",
+        Description = "Criterion that matches type and version of the user's browser",
+        LanguagePath = "/shell/cms/visitorgroups/criteria/browser")]
+    public class BrowserCriterion : CriterionBase<BrowserModel>
+    {
+        public override bool IsMatch(IPrincipal principal, HttpContextBase httpContext)
+        {
+            return MatchBrowserType(httpContext.Request.Browser.Browser) &&
+                   MatchBrowserVersion(httpContext.Request.Browser.MajorVersion);
+        }
+
+        protected virtual bool MatchBrowserVersion(int majorVersion)
+        {
+            switch (Model.Condition)
+            {
+                case CompareCondition.LessThan:
+                    return majorVersion < Model.MajorVersion;
+                case CompareCondition.Equal:
+                    return majorVersion == Model.MajorVersion;
+                case CompareCondition.GreaterThan:
+                    return majorVersion > Model.MajorVersion;
+                case CompareCondition.NotEqual:
+                    return majorVersion != Model.MajorVersion;
+                default:
+                    return false;
+            }
+        }
+
+        protected virtual bool MatchBrowserType(string browserType)
+        {
+            browserType = (browserType ?? String.Empty).ToLower();
+            if (browserType.Equals("ie") || browserType.Equals("internetexplorer"))
+            {
+                return Model.Browser == BrowserType.IE;
+            }
+            if (browserType.Equals("firefox"))
+            {
+                return Model.Browser == BrowserType.FireFox;
+            }
+
+            if (browserType.Equals("chrome"))
+            {
+                return Model.Browser == BrowserType.Chrome;
+            }
+
+            return Model.Browser == BrowserType.Other;
+        }
+    }
+}
---
+++ ExtendingEditUi/Models/VisitorGroupCriterion/BrowserModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Filters;
+using EPiServer.Personalization.VisitorGroups;
+using EPiServer.Web.Mvc.VisitorGroups;
+
+namespace ExtendingEditUi.Models.VisitorGroupCriterion
+{
+    public class BrowserModel : CriterionModelBase
+    {
+        [DojoWidget(
+        SelectionFactoryType = typeof(EnumSelectionFactory),
+        LabelTranslationKey = "/shell/cms/visitorgroups/criteria/browser/browsertype",
+        AdditionalOptions = "{ selectOnClick: true }"),
+        Required]
+        public BrowserType Browser { get; set; }
+
+        [DojoWidget(
+            SelectionFactoryType = typeof(EnumSelectionFactory),
+            LabelTranslationKey = "/shell/cms/visitorgroups/criteria/browser/comparecondition",
+            AdditionalOptions = "{ selectOnClick: true }"),
+            Required]
+        public CompareCondition Condition { get; set; }
+        [DojoWidget(
+            DefaultValue = 0,
+            LabelTranslationKey = "/shell/cms/visitorgroups/criteria/browser/majorversion",
+            AdditionalOptions = "{ constraints: {min: 0}, selectOnClick: true }"),
+            Range(0, 0xff)]
+        public int MajorVersion { get; set; }
+
+        public override ICriterionModel Copy()
+        {
+            return ShallowCopy();
+        }
+    }
+
+    public enum BrowserType
+    {
+        IE,
+        FireFox,
+        Chrome,
+        Other
+    }
+}
